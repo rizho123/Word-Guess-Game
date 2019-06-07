91ae8695ee293c4b229ff1c6217137acc9cbbd35
Richard Zhou (Insight Global Inc) <v-riczho@microsoft.com>
2019-06-07 03:33:32 -0700

    Updating index with basic structure of game.  Updated game code

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+var words = ["bronze", "monkey", "random", "difficult", "unable", "simple"]
+
+var currentWord = "";
+var userWins = 0;
+var userLosses = 0;
+var wordLetters = [];
+var blanks = 0;
+var blankPlacer = [];
+var guessRemain = 5;
+var wrongGuess = [];
+
+
+function Game() {
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    wordLetters = currentWord.split("");
+    blanks = wordLetters.length;
+    
+    for (var i = 0; i < blanks; i++) {
+        blankPlacer.push("_");
+    }
+
+    document.getElementById("currentword").innerHTML = "  " + blankPlacer.join("  ");
+}
+
+function reset() {
+    guessRemain = 5;
+    wrongGuess = [];
+    blankPlacer = [];
+    Game()
+}
+
+function checkLetters(letter) {
+    wordL = false;
+    for (var i = 0; i < blanks; i++) {
+        if (currentWord[i] === letter) {
+            wordL = true;
+        }
+    }
+    if (wordL) {
+        for (var i = 0; i < blanks; i++) {
+            if (currentWord[i] === letter) {
+                blankPlacer[i] = letter;
+            }
+        }
+    }
+    else {
+        wrongGuess.push(letter);
+        guessRemain--;
+    }
+}
+
+function complete() {
+    console.log(wordLetters, blankPlacer);
+    if (wordLetters.toString() == blankPlacer.toString()) {
+        userWins++;
+        reset()
+        document.getElementById("wins").innerHTML = " " + userWins;
+
+    } else if (guessRemain === 0) {
+        userLosses++;
+        reset()
+        document.getElementById("losses").innerHTML = " " + userLosses;
+    }
+    document.getElementById("currentword").innerHTML = "  " + blankPlacer.join(" ");
+    document.getElementById("gremain").innerHTML = " " + guessRemain;
+}
+
+Game()
+
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(guesses);
+    complete()
+    document.getElementById("lguessed").innerHTML = "  " + wrongGuess.join(" ");
+};
