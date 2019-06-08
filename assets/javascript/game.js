60ceb0351f1b16f6b10080aa4e29202b1dfdd687
Richard Zhou (Insight Global Inc) <v-riczho@microsoft.com>
2019-06-08 01:56:00 -0700

    updated css and html to include images and captions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var words = ["bronze", "monkey", "random", "difficult", "unable", "simple"]
+var words = ["nebula", "monkey", "planets", "milkyway", "galaxy", "solarflare"]
 
 var currentWord = "";
 var userWins = 0;
@@ -6,7 +6,7 @@ var userLosses = 0;
 var wordLetters = [];
 var blanks = 0;
 var blankPlacer = [];
-var guessRemain = 5;
+var guessRemain = 10;
 var wrongGuess = [];
 
 
@@ -22,8 +22,30 @@ function Game() {
     document.getElementById("currentword").innerHTML = "  " + blankPlacer.join("  ");
 }
 
+function imgBrd() {
+    if (currentWord === words[0]) {
+        document.getElementById("image").src = "./assets/images/imageboard/nebula.gif";
+        document.getElementById("caption").innerHTML = "Nebula";
+    } else if (currentWord === words[1]) {
+        document.getElementById("image").src = "./assets/images/imageboard/monkey.gif";
+        document.getElementById("caption").innerHTML = "Monkey";
+    } else if (currentWord === words[2]) {
+        document.getElementById("image").src = "./assets/images/imageboard/planets.gif";
+        document.getElementById("caption").innerHTML = "Planets";
+    } else if (currentWord === words[3]) {
+        document.getElementById("image").src = "./assets/images/imageboard/milkyway.gif";
+        document.getElementById("caption").innerHTML = "Milky Way";
+    } else if (currentWord === words[4]) {
+        document.getElementById("image").src = "./assets/images/imageboard/galaxy.gif";
+        document.getElementById("caption").innerHTML = "Galaxy";
+    } else if (currentWord === words[5]) {
+        document.getElementById("image").src = "./assets/images/imageboard/solarflare.gif";
+        document.getElementById("caption").innerHTML = "Solar Flare";
+    }
+}
+
 function reset() {
-    guessRemain = 5;
+    guessRemain = 10;
     wrongGuess = [];
     blankPlacer = [];
     Game()
@@ -53,11 +75,14 @@ function complete() {
     console.log(wordLetters, blankPlacer);
     if (wordLetters.toString() == blankPlacer.toString()) {
         userWins++;
+        imgBrd()
         reset()
         document.getElementById("wins").innerHTML = " " + userWins;
         document.getElementById("status").innerHTML = "You win!";
         setTimeout(function(){
             document.getElementById("status").innerHTML = "Press any key to begin!";
+            document.getElementById("image").src = "./assets/images/imageboard/starterpic.gif";
+            document.getElementById("caption").innerHTML = "";
         }, 3000)
 
     } else if (guessRemain === 0) {
@@ -67,6 +92,8 @@ function complete() {
         document.getElementById("status").innerHTML = "You lose!";
         setTimeout(function(){
             document.getElementById("status").innerHTML = "Press any key to begin!";
+            document.getElementById("image").src = "./assets/images/imageboard/starterpic.gif";
+            document.getElementById("caption").innerHTML = "";
         }, 3000)
     }
     document.getElementById("currentword").innerHTML = "  " + blankPlacer.join(" ");
